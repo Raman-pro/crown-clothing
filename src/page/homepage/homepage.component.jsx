8e1c252d2c0b196af984b64f2c7281ff008b8e1c
Raman-pro <ramanjot2007@outlook.com>
2020-09-12 12:26:53 +0530

    broken our homepage into smaller components and menu items finished

---
+++ src/page/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.style.scss';
+import Directory from '../../Components/directory/directory.component'
+
+const HomePage = () => (
+    // eslint-disable-next-line no-unused-expressions
+    <div className="home-page">
+        <Directory/>
+    </div>
+)
+
+export default HomePage;
