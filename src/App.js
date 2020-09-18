914fb34eed1778975a19af47af3b5ac0be0617c7
Raman-pro <ramanjot2007@outlook.com>
2020-09-18 15:33:13 +0530

    Header added with crown.svg.Crown.svg is inside assets

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import HomePage from './page/homepage/homepage.component';
 import {Switch, Route} from 'react-router-dom';
 import ShopPage from './page/shop/shop.component';
-
 import './App.css'
+import Header from './Components/header/header.component';
+
 
 function App() {
     return (
         <div>
+            <Header/>
             <Switch>
                 <Route exact path='/' component={HomePage}/>
                 <Route path='/shop' component={ShopPage}/>
